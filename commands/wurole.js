a7f9a735fdd0dd730b45eb7b6470b63c66bed3ca
Shadow <william.shadow@outlook.com>
2021-02-17 08:06:39 +0000

    command stuff

--- commands/wurole.js
+++ commands/wurole.js
@@ -22,24 +22,30 @@ module.exports = {
 
     let fields = [
       "Name",
-      "Aura",
+      {var: "Aura", allowed: ["good", "evil", "unknown"]},
       "Team",
       "Icon Description",
-      "Replaces",
+      {var: "Replaces", q: "What does this role replace?"},
       "Win Condition",
       "Ability",
       "In-game Description",
-      "Interactions",
-      "Pros",
-      "Cons",
+      {var: "Interactions", q: "What are the interactions of this role?"},
+      {var: "Pros", q: "What are the pros of this role?"},
+      {var: "Cons", q: "What are the cons of this role?"},
+      {var: "Additional Info", q: "Any additional info? Reply with the info or say \"None\""}
     ]
-    let data = {}
+    let data = {}, q = "", allowed = []
     for await (let x of fields) {
+      if(typeof x == "object") {
+        q = x.q ? x.q : "", allowed = x.allowed ? x.allowed : [], x = x.var
+      }
+      while(!data[x]){
       let prompt = await message.author.send(
         new Discord.MessageEmbed()
           .setTitle(x)
-          .setDescription(`What is the ${x} of this role?`)
+          .setDescription(q ? q : `What is the ${x} of this role?`.toLowerCase())
       )
+      q = ""
 
       let res = await prompt.channel
         .awaitMessages((msg) => msg.author.id == message.author.id, {
@@ -54,8 +60,16 @@ module.exports = {
             .setColor("RED")
             .setTitle("Prompt timed out.")
         )
+      res.first().delete()
+      prompt.delete()
       res = res.first().content
-      data[x] = res
+
+      if(allowed.length > 0 && !allowed.includes(res.toLowerCase())) {
+        message.channel.send(`That isn't a valid ${x}!`.toLowerCase()).then(m => setTimeout(() => m.delete(), 10000))
+      } else {
+        data[x] = res
+      }
+        }
     }
 
     let descformatted = `**Name**: ${data["Name"]}
@@ -63,20 +77,34 @@ module.exports = {
 **Team**: ${data["Team"]}
 **Icon**: ${data["Icon Description"]}
 **Replaces**: ${data["Replaces"]}
-**Win Condition**: ${data["Win Condition"]}\x0A\x0A### Ability
-${data["Ability"]}\x0A\x0A### In-game Description
-${data["In-Game Description"]}\x0A\x0A### Interactions
-${data["Interactions"]}\x0A\x0A### Pros
-${data["Pros"]}\x0A\x0A### Cons
-${data["Cons"]}`
-    message.channel.send(descformatted)
+**Win Condition**: ${data["Win Condition"]}
+
+**Ability**
+${data["Ability"]}
+
+**In-game Description**
+${data["In-game Description"]}
+
+**Interactions**
+${data["Interactions"]}
+
+**Pros**
+${data["Pros"]}
+
+**Cons**
+${data["Cons"]}
+
+**Other Information**
+${data["Other Information"]}`
+
+    //message.channel.send(descformatted)
     let req =
       "/cards?idList=" +
       tconfig.lists.writersutopia +
       "&idLabels=" +
       [tconfig.labels.wureview] +
       "&name=" +
-      data.name +
+      data["Name"] +
       "&desc=" +
       descformatted +
       "&pos=top"
